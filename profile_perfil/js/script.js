cf2bbbe4f130a970372a706ad0447e58726c3444
arthur-deneigre <134006275+arthur-deneigre@users.noreply.github.com>
2023-10-03 16:48:37 -0300

    03/10

--- profile_perfil/js/script.js
+++ profile_perfil/js/script.js
@@ -1,6 +1,33 @@
-function mode(params) {
+function mode() {
 
     const html = document.documentElement
-    
-    html.classList.mode()
+    html.classList.toggle("escuro")
+
+    const img = document.querySelector(".profile img")
+
+    if(html.classList.contains("escuro")) {
+        document.body.style.backgroundImage="url('./img/abbr-escuro.png')"
+        img.setAttribute("src", "././img/logo2.jpg");
+        
+    } else {
+        document.body.style.backgroundImage="url('./img/abbr-claro.png')"
+        img.setAttribute("src", "././img/logo1.jpg")
+    }
+    console.log(html)
 }
+
+const btn = document.getElementById('btn');
+let movimentar = false;
+
+btn.addEventListener('click', () => {
+    if (movimentar) {
+        btn.style.left = '0';
+    } else {
+        btn.style.left = '55px'; // Ou a quantidade desejada de movimento
+    }
+    movimentar = !movimentar;
+});
+
+
+
+
